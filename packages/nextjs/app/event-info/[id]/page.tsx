bf907588ceeddd4d411e75bd68c54a0cbf551d8a
brepositorium <rcryptob@protonmail.ch>
2024-05-29 23:28:15 +0300

    redesign event info page

--- packages/nextjs/app/event-info/[id]/page.tsx
+++ packages/nextjs/app/event-info/[id]/page.tsx
@@ -1,9 +1,13 @@
 "use client"
 import { useEffect, useState } from "react";
 import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
-import { fetchExtraDetails, getBalanceOf} from "~~/utils/chain-of-events/deployContract";
+import { fetchExtraDetails, getUnredeemedBalanceOf} from "~~/utils/chain-of-events/deployContract";
 import { useAccount } from 'wagmi';
 import Link from "next/link";
+import { ApolloClient, InMemoryCache, useQuery } from "@apollo/client";
+import { GET_EVENT_DETAILS_BY_ID } from "~~/utils/chain-of-events/queries";
+import ExtraCard from "~~/components/ExtraCard";
+import { ACTIONS } from "~~/utils/chain-of-events/Actions";
 
 type PageProps = {
   params: { id: number };
@@ -12,14 +16,25 @@ type PageProps = {
 interface ExtraDetail {
     balance: string;
     extraType?: bigint;
+    extraAddress: string;
     name?: string;
     symbol?: string;
+    description?: string;
+    imageUrl?: string;
     price?: bigint;
     uri?: string;
 }
 
+//TODO: move from here
+const client = new ApolloClient({
+    uri: "https://api.studio.thegraph.com/query/71641/test-coe/version/latest",
+    cache: new InMemoryCache()
+  });
+
 const EditDashboardPage = ({ params }: PageProps) => {
     const id = params.id;
+    const { loading: loadingEvents, error: errorEvents, data: dataEvents } = useQuery(GET_EVENT_DETAILS_BY_ID, {
+        variables: { id }, client: client});
 
     const { address } = useAccount()
     
@@ -28,7 +43,7 @@ const EditDashboardPage = ({ params }: PageProps) => {
         functionName: "getExtras",
         args: [BigInt(id)]
     });
-
+    
     const [extraDetails, setExtraDetails] = useState<ExtraDetail[]>([]);
     
     useEffect(() => {
@@ -38,17 +53,18 @@ const EditDashboardPage = ({ params }: PageProps) => {
                 return;
             }
 
-            const balancePromises = data.map(extra => getBalanceOf(extra, address!));
+            const balancePromises = data.map(extra => getUnredeemedBalanceOf(extra, address!));
             const balances = await Promise.all(balancePromises);
     
             const nonZeroExtras = data.filter((extra, index) => BigInt(balances[index]) > 0);
     
             const detailPromises = nonZeroExtras.map(extra => fetchExtraDetails(extra));
             const details = await Promise.all(detailPromises);
-    
+            
             const combinedDetails = nonZeroExtras.map((extra, index) => ({
                 ...details[index],
-                balance: balances[data.indexOf(extra)]
+                balance: balances[data.indexOf(extra)],
+                extraAddress: extra
             }));
             setExtraDetails(combinedDetails);
         };
@@ -59,21 +75,44 @@ const EditDashboardPage = ({ params }: PageProps) => {
 
     }, [data, address]);
     
+    if (loadingEvents) return <div className="flex justify-center loading loading-spinner loading-lg"></div>;
+    if (errorEvents) return <p>Error loading events</p>;
+
   return (
-    <div>
-        <Link href={"/shop/" + id + "/0"} className="btn">Buy ticket</Link>
-        <Link href={"/shop/" + id + "/1"} className="btn">Buy drink/snack</Link>
-        <div>
-            {extraDetails.length > 0 ? extraDetails.map((detail, index) => (
-                <div key={index} className="detail-item">
-                    <p>Name: {detail?.name}</p>
-                    <p>Symbol: {detail?.symbol}</p>
-                    <p>URI: {detail?.uri}</p>
-                    <p>Price: {Number(detail?.price)}</p>
-                    <p>Balance: {Number(detail?.balance)}</p>
-                </div>
-            )) : <p>No extra details available.</p>}
+    <div className="h-[650px] bg-circles bg-no-repeat">
+      <div className="flex flex-col items-center mt-12 gap-4">
+        <div className="text-2xl font-extrabold mb-6">
+          {dataEvents.eventCreateds[0].createdEvent_name}
         </div>
+        <div className="text-lg w-96 border p-2">
+            {dataEvents.eventCreateds[0].createdEvent_description}
+        </div>
+        <div className="text-lg w-96 border p-2">
+            {dataEvents.eventCreateds[0].createdEvent_location}
+        </div>
+      </div>
+      <div className="flex my-8 justify-center">
+        <Link href={"/shop/" + id + "/0"} className="btn btn-gradient-primary rounded ml-8">Buy ticket</Link>
+        <Link href={"/shop/" + id + "/1"} className="btn btn-gradient-primary rounded ml-8">Buy drink/snack</Link>
+      </div>
+      <div className="container mx-auto px-40">
+        <h1 className="text-2xl font-bold my-4">Your assets</h1>
+        <div className="grid grid-cols-3 gap-4">
+            {extraDetails.length > 0 ? extraDetails.map((detail, index) => (
+              <ExtraCard
+                extraName={detail.name}
+                description={detail.description}
+                imageUrl={detail.imageUrl}
+                extraAddress={detail.extraAddress}
+                price={Number(detail.price)}
+                hasQuantity={true}
+                noOfItems={Number(detail.balance)}
+                action={ACTIONS.TRANSFER}
+                extraType={Number(detail.extraType)}
+              />
+            )): <p>No extra details available.</p>}
+        </div>
+      </div>
     </div>
   );
 };
