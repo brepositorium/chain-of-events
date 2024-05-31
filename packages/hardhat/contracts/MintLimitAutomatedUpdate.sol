1f63aec6988bd8ad8ae99a912c20f4a4faccacbd
brepositorium <rcryptob@protonmail.ch>
2024-05-31 20:38:38 +0300

    add automation contracts for mint limit and pause

---
+++ packages/hardhat/contracts/MintLimitAutomatedUpdate.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import { AutomationCompatibleInterface } from "@chainlink/contracts/src/v0.8/automation/AutomationCompatible.sol";
+import { LinkTokenInterface } from "@chainlink/contracts/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
+import "./ExtraNft.sol";
+
+struct RegistrationParams {
+	string name;
+	bytes encryptedEmail;
+	address upkeepContract;
+	uint32 gasLimit;
+	address adminAddress;
+	uint8 triggerType;
+	bytes checkData;
+	bytes triggerConfig;
+	bytes offchainConfig;
+	uint96 amount;
+}
+
+interface AutomationRegistrarInterface {
+	function registerUpkeep(
+		RegistrationParams calldata requestParams
+	) external returns (uint256);
+}
+
+contract MintLimitAutomatedUpdate is AutomationCompatibleInterface {
+	address public admin;
+	ExtraNft public extraContract;
+	uint256 public scheduledTime;
+	uint256 public newMintLimit;
+	LinkTokenInterface public immutable i_link;
+	AutomationRegistrarInterface public immutable i_registrar;
+
+	constructor(
+		address _extraContract,
+		LinkTokenInterface link,
+		AutomationRegistrarInterface registrar
+	) {
+		admin = msg.sender;
+		extraContract = ExtraNft(_extraContract);
+		i_link = link;
+		i_registrar = registrar;
+	}
+
+	function getContractBalance() external view returns (uint256) {
+		return i_link.balanceOf(address(this));
+	}
+
+	function scheduleMintLimitUpdate(
+		uint256 _newMintLimit,
+		uint256 _time
+	) external {
+		require(msg.sender == admin, "Unauthorized");
+		newMintLimit = _newMintLimit;
+		scheduledTime = _time;
+	}
+
+	function checkUpkeep(
+		bytes calldata
+	) external view override returns (bool upkeepNeeded, bytes memory) {
+		upkeepNeeded = (block.timestamp >= scheduledTime && scheduledTime != 0);
+	}
+
+	function performUpkeep(bytes calldata) external override {
+		require(
+			block.timestamp >= scheduledTime,
+			"Too early to update mint limit"
+		);
+		extraContract.updateMintLimit(newMintLimit);
+		scheduledTime = 0;
+	}
+
+	function registerUpkeep(uint96 amount) external returns (uint256) {
+		RegistrationParams memory params = RegistrationParams({
+			name: extraContract.name(),
+			encryptedEmail: "0x",
+			upkeepContract: address(this),
+			gasLimit: 500000,
+			adminAddress: msg.sender,
+			triggerType: 0,
+			checkData: "0x",
+			triggerConfig: "0x",
+			offchainConfig: "0x",
+			amount: amount
+		});
+
+		i_link.approve(address(i_registrar), params.amount);
+
+		return i_registrar.registerUpkeep(params);
+	}
+}
